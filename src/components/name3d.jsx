7f7d55e6271c4bb0409d9b0890f6f0e2d629cab4
Johan Nilsson <johan.nilsson.mail@gmail.com>
2024-01-02 18:53:35 +0100

    fix 3d dimension

--- src/components/name3d.jsx
+++ src/components/name3d.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable react/no-unknown-property */
 import { Canvas, useFrame, useThree } from '@react-three/fiber';
-import { Center, Text3D, Float, Resize } from '@react-three/drei';
+import { Center, Text3D, Float } from '@react-three/drei';
 import { useRef } from 'react';
 
 const CustomMesh = () => {
@@ -17,55 +17,53 @@ const CustomMesh = () => {
 
   return (
     <>
-      <Resize>
-        <Center>
-          <group ref={ref} position={(-8, -10, 2)}>
-            <Float
-              speed={2} // Animation speed, defaults to 1
-              rotationIntensity={0.8} // XYZ rotation intensity, defaults to 1
-              floatIntensity={0.1} // Up/down float intensity, works like a multiplier with floatingRange,defaults to 1
-              floatingRange={[2, 1.5]} // Range of y-axis values the object will float within, defaults to [-0.1,0.1]
-            >
-              <mesh>
-                <Text3D
-                  bevelSegments={1}
-                  bevelThickness={0.3}
-                  bevelEnabled
-                  curveSegments={10}
-                  size={2.5}
-                  lineHeight={1}
-                  letterSpacing={0.1}
-                  font={'./JHreverse.json'}>
-                  {`Johan Nilsson`}
-                  <meshPhysicalMaterial
-                    roughness={0.1}
-                    transmission={0.05}
-                    thickness={5}
-                  />
-                </Text3D>
-              </mesh>
-              <mesh position={[9.5, -3, -1.5]}>
-                <Text3D
-                  bevelSegments={1}
-                  bevelThickness={0.3}
-                  bevelEnabled
-                  curveSegments={10}
-                  size={1}
-                  lineHeight={0.5}
-                  letterSpacing={0.1}
-                  font={'./Payback.json'}>
-                  {`Portfolio`}
-                  <meshPhysicalMaterial
-                    roughness={0.1}
-                    transmission={0.05}
-                    thickness={5}
-                  />
-                </Text3D>
-              </mesh>
-            </Float>
-          </group>
-        </Center>
-      </Resize>
+      <Center>
+        <group ref={ref} position={(-8, -10, 2)}>
+          <Float
+            speed={2} // Animation speed, defaults to 1
+            rotationIntensity={0.8} // XYZ rotation intensity, defaults to 1
+            floatIntensity={0.1} // Up/down float intensity, works like a multiplier with floatingRange,defaults to 1
+            floatingRange={[2, 1.5]} // Range of y-axis values the object will float within, defaults to [-0.1,0.1]
+          >
+            <mesh>
+              <Text3D
+                bevelSegments={1}
+                bevelThickness={0.3}
+                bevelEnabled
+                curveSegments={10}
+                size={2.5}
+                lineHeight={1}
+                letterSpacing={0.1}
+                font={'./JHreverse.json'}>
+                {`Johan Nilsson`}
+                <meshPhysicalMaterial
+                  roughness={0.1}
+                  transmission={0.05}
+                  thickness={5}
+                />
+              </Text3D>
+            </mesh>
+            <mesh position={[9.5, -3, -1.5]}>
+              <Text3D
+                bevelSegments={1}
+                bevelThickness={0.3}
+                bevelEnabled
+                curveSegments={10}
+                size={1}
+                lineHeight={0.5}
+                letterSpacing={0.1}
+                font={'./Payback.json'}>
+                {`Portfolio`}
+                <meshPhysicalMaterial
+                  roughness={0.1}
+                  transmission={0.05}
+                  thickness={5}
+                />
+              </Text3D>
+            </mesh>
+          </Float>
+        </group>
+      </Center>
     </>
   );
 };
